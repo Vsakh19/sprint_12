4c95649581c499e7a3bffdc55ef63fa5e31a02af
Vladislav Sakharov <brodron@gmail.com>
2019-12-09 03:35:21 +0300

    Fourth submit

--- routes/cards.js
+++ routes/cards.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cards = fs.readFileSync(path.join(__dirname, '../data/cards.json'), { encoding: 'utf8' });
 const router = require('express').Router();
 
-router.get('/cards', (req, res) => {
+router.get('/', (req, res) => {
   res.json(cards);
 });
 
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 
 const path = require('path');
 
-const users = fs.readFileSync(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' });
+const users = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' }));
 const router = require('express').Router();
 
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
   res.json(users);
 });
 
-router.get('/users/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params;
   for (let i = 0; i < users.length; i += 1) {
     // eslint-disable-next-line no-underscore-dangle
