3eba1ce4bc50f6e67c5d1643308d12766888ab9d
Vladislav Sakharov <brodron@gmail.com>
2020-02-06 19:39:08 +0300

    sprint 15 v7

--- routes/cards.js
+++ routes/cards.js
@@ -6,8 +6,8 @@ const auth = require('../middlewares/auth');
 router.get('/', auth, getCards);
 router.post('/', auth, celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required(),
-    link: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(new RegExp('https?://(www.)?[-a-zA-Z0-9@:%.+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%+.~#?&//=]*)')),
   }),
 }), createCard);
 router.delete('/:cardId', auth, celebrate({
